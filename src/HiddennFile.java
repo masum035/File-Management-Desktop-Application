99bf63a72e0fa2e232cc27b9c563f0dea0b3c7f9
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2020-10-18 02:29:38 +0600

    9th commit

--- src/HiddennFile.java
+++ src/HiddennFile.java
@@ -1,4 +1,12 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.attribute.DosFileAttributes;
 
 public class HiddennFile {
 	private JTextField textField1;
@@ -14,5 +22,25 @@ public class HiddennFile {
 		HiddenFrame.setSize(680,170);
 		HiddenFrame.setLocationRelativeTo(null);
 		HiddenFrame.setVisible(true);
+		hideItButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String x = textField1.getText();
+				try {
+					HideFileFunction(Path.of(x));
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				}
+			}
+		});
 	}
+	public static void HideFileFunction(Path filePath) throws IOException {
+		DosFileAttributes attr = Files.readAttributes(filePath,DosFileAttributes.class);
+		System.out.println(filePath.getFileName() + "\nCreation time : " + attr.creationTime() + "\nLast Modification Time : "+ attr.lastModifiedTime() + "\nFile Size : " + attr.size() +"kb"+ "\nHidden attribute :" + attr.isHidden());
+		Files.setAttribute(filePath, "dos:hidden", true, LinkOption.NOFOLLOW_LINKS);
+		attr = Files.readAttributes(filePath,DosFileAttributes.class);
+		System.out.println(filePath.getFileName() + "\nCreation time : " + attr.creationTime() + "\nLast Modification Time : "+ attr.lastModifiedTime() + "\nFile Size : " + attr.size() + "kb" + "\nHidden attribute :" + attr.isHidden());
+	}
+
 }
+//File + Folder hide is done But only one should remain,only jfilechooser
--- src/LockTheFile.java
+++ src/LockTheFile.java
@@ -1,13 +1,25 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
+import java.nio.file.Path;
+import java.util.concurrent.TimeUnit;
 
 public class LockTheFile {
 	private JPanel LockFilePanel;
 	private JButton fileChooserButton;
 	private JTextField textField1;
-	private JTextField textField2;
+	private JTextField textField_dir;
 	private JButton secureLockButton;
 	private JButton justLockItButton;
 	private JPasswordField passwordField1;
+	private JButton readOnlyButton;
 
 	public LockTheFile(){
 		JFrame LockFrame = new JFrame();
@@ -16,6 +28,47 @@ public class LockTheFile {
 		LockFrame.setLocationRelativeTo(null);
 		LockFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
 		LockFrame.setVisible(true);
+		justLockItButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String fileLocAtion = textField_dir.getText();
+				try {
+					FileLockFuntionusingFileChannel(Path.of(fileLocAtion));
+				} catch (FileNotFoundException fileNotFoundException) {
+					fileNotFoundException.printStackTrace();
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				}
+			}
+		});
+		readOnlyButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String fileLoCation = textField_dir.getText();
+				fileLockFunctionReadOnly(Path.of(fileLoCation));
+			}
+		});
+	}
+	public static void fileLockFunctionReadOnly(Path filePath){
+		File file = new File(String.valueOf(filePath));
+		boolean success = /*file.setReadOnly(); */ file.setWritable(false);
+		if(success) {
+			System.out.println("File marked as read only");
+		} else {
+			System.out.println("File could not be marked as read only");
+		}
+
+	}
+	public static void FileLockFuntionusingFileChannel(Path filePath) throws IOException {
+		FileChannel channel = new RandomAccessFile(String.valueOf(filePath), "rw").getChannel();
+		FileLock lock = channel.lock();
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		lock.release();
+		channel.close();
 	}
 
 }
