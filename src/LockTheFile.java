00f209caf35bf9bc20a537b27cfd2ed93a33ea95
Abdullah Al Masum <abdullahmasum6035@gmail.com>
2020-11-10 20:36:07 +0600

    17th commit

--- src/LockTheFile.java
+++ src/LockTheFile.java
@@ -5,6 +5,10 @@ import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.model.enums.EncryptionMethod;
 
 import javax.swing.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -14,65 +18,91 @@ import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class LockTheFile {
+public class LockTheFile implements DropTargetListener {
 	private JPanel LockFilePanel;
-	private JButton fileChooserButton;
-	private JTextField textField1;
+	private JTextArea justDragDropAnyTextArea;
+	private JButton justLockButton;
+	private JButton securedLockButton;
+	private JLabel infoAboutZipping;
 	private JTextField textField_dir;
-	private JButton secureLockButton;
 	private JButton justLockItButton;
-	private JPasswordField passwordField1;
-	private JButton readOnlyButton;
+
+	ArrayList<String> pathArray = new ArrayList<>();
 
 	public LockTheFile() {
+		DropTarget dt = new DropTarget(justDragDropAnyTextArea, this);
 		JFrame LockFrame = new JFrame();
 		LockFrame.add(LockFilePanel);
 		LockFrame.setSize(680, 210);
 		LockFrame.setLocationRelativeTo(null);
 		LockFrame.setDefaultCloseOperation(WindowConstants.HIDE_ON_CLOSE);
 		LockFrame.setVisible(true);
-		justLockItButton.addActionListener(new ActionListener() {
+//		justLockItButton.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				String fileLocAtion = textField_dir.getText();
+//				try {
+//					FileLockFuntionusingFileChannel(Path.of(fileLocAtion));
+//				} catch (FileNotFoundException fileNotFoundException) {
+//					fileNotFoundException.printStackTrace();
+//				} catch (IOException ioException) {
+//					ioException.printStackTrace();
+//				}
+//			}
+//		});
+		justLockButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String fileLocAtion = textField_dir.getText();
-				try {
-					FileLockFuntionusingFileChannel(Path.of(fileLocAtion));
-				} catch (FileNotFoundException fileNotFoundException) {
-					fileNotFoundException.printStackTrace();
-				} catch (IOException ioException) {
-					ioException.printStackTrace();
+				for (int i = 0; i < pathArray.size(); i++) {
+					String fileLoCation = pathArray.get(i);
+					File file = new File(fileLoCation);
+					if (!file.isDirectory()) {
+						fileLockFunctionReadOnly(fileLoCation);
+					} else {
+						JOptionPane.showMessageDialog(null, "Folder can not be just locked.Please Drop File/Files", "Just Lock Failed", JOptionPane.ERROR_MESSAGE);
+					}
 				}
 			}
 		});
-		readOnlyButton.addActionListener(new ActionListener() {
+		securedLockButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String fileLoCation = textField_dir.getText();
-				fileLockFunctionReadOnly(Path.of(fileLoCation));
-			}
-		});
-		secureLockButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String fileLoCation = textField_dir.getText();
-				secureLock(fileLoCation);
+				//String fileLoCation = textField_dir.getText();
+				for (int i = 0; i < pathArray.size(); i++) {
+					String fileLoCation = pathArray.get(i);
+					File file = new File(fileLoCation);
+					if (!file.isDirectory()) {
+						if(secureLock(fileLoCation)){
+							JOptionPane.showInternalMessageDialog(null, "Your File has been Successfully Secured", "Yess! Success!!", JOptionPane.INFORMATION_MESSAGE);
+						}else {
+							JOptionPane.showInternalMessageDialog(null, "One of the File here is already a zipped File\nDrop another file/files", "Sorry :'(", JOptionPane.ERROR_MESSAGE);
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Folder can not be securely locked.Please Drop File/Files", "Secure Lock Failed", JOptionPane.ERROR_MESSAGE);
+					}
+				}
 			}
 		});
 	}
 
-	public static void secureLock(String fileLocation) {
+	public static boolean secureLock(String fileLocation) {
 		// TODO: 10/18/2020 choose your zip file location & delete that previous file & set passwordField
 
+		if (fileLocation.contains(".zip")) {
+			return false;
+		}
 		int lastSlashPosition = fileLocation.lastIndexOf('\\');
 		String zipFileLocation = fileLocation.substring(0, lastSlashPosition);
-		String zipFileName = zipFileLocation + "\\MasumProduction.zip";
+		String zipFileName = zipFileLocation + "\\" + "MasumProduction.zip";
 
-		if(fileLocation.contains(".")){
+		if (fileLocation.contains(".")) {
 			int lastDotPosition = fileLocation.lastIndexOf('.');
-			String zipFileNaame = fileLocation.substring(lastSlashPosition+1 , lastDotPosition);
-			zipFileName = zipFileLocation +"\\"+ zipFileNaame + ".zip";
+			String zipFileNaame = fileLocation.substring(lastSlashPosition + 1, lastDotPosition);
+			zipFileName = zipFileLocation + "\\" + zipFileNaame + ".zip";
 		}
 
 		String password = "abc";
@@ -83,7 +113,6 @@ public class LockTheFile {
 			net.lingala.zip4j.ZipFile zipFile = new net.lingala.zip4j.ZipFile(zipFileName, password.toCharArray());
 			zipFile.addFile(new File(fileLocation), zipParameters);
 
-			JOptionPane.showInternalMessageDialog(null,"Your File has been Successfully Secured","Yess! Success!!",JOptionPane.INFORMATION_MESSAGE);
 		} catch (ZipException e) {
 			e.printStackTrace();
 		}
@@ -93,20 +122,23 @@ public class LockTheFile {
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		}
-
+		return true;
 	}
 
-	public static void fileLockFunctionReadOnly(Path filePath) {
-		File file = new File(String.valueOf(filePath));
+	public static void fileLockFunctionReadOnly(String filePath) {
+		int lastSlashPosition = filePath.lastIndexOf('\\');
+//		int lastDotPosition = filePath.lastIndexOf('.');
+		String filename = filePath.substring(lastSlashPosition+1,filePath.length());
+		File file = new File(filePath);
 		boolean success = /*file.setReadOnly(); */ file.setWritable(false);
 		if (success) {
-			System.out.println("File marked as read only");
+			JOptionPane.showMessageDialog(null,"Your File '"+ filename + "' has been locked","Locked Successfully",JOptionPane.INFORMATION_MESSAGE);
 		} else {
-			System.out.println("File could not be marked as read only");
+			JOptionPane.showMessageDialog(null,"Your File '"+ filename + "' could not be Locked.Try Secure Lock","Locked Failed",JOptionPane.ERROR_MESSAGE);
 		}
 
 	}
-
+//this is for vanish mode
 	public static void FileLockFuntionusingFileChannel(Path filePath) throws IOException {
 		FileChannel channel = new RandomAccessFile(String.valueOf(filePath), "rw").getChannel();
 		FileLock lock = channel.lock();
@@ -119,4 +151,57 @@ public class LockTheFile {
 		channel.close();
 	}
 
+	@Override
+	public void dragEnter(DropTargetDragEvent dtde) {
+
+	}
+
+	@Override
+	public void dragOver(DropTargetDragEvent dtde) {
+
+	}
+
+	@Override
+	public void dropActionChanged(DropTargetDragEvent dtde) {
+
+	}
+
+	@Override
+	public void dragExit(DropTargetEvent dtde) {
+
+	}
+
+	@Override
+	public void drop(DropTargetDropEvent event) {
+		justDragDropAnyTextArea.append("\n");
+		//chooseFolderButtonForDragAndDrop.setVisible(true);
+		infoAboutZipping.setVisible(true);
+
+		try {
+			Transferable tr = event.getTransferable();
+			DataFlavor[] flavors = tr.getTransferDataFlavors();
+			for (int i = 0; i < flavors.length; i++) {
+				if (flavors[i].isFlavorJavaFileListType()) {
+					event.acceptDrop(DnDConstants.ACTION_COPY);
+					List list = (List) tr.getTransferData(flavors[i]);
+					for (int j = 0; j < list.size(); j++) {
+						//textAreaForDragandDrop.append(list.get(j) + "\n");
+						justDragDropAnyTextArea.append("File/Folder has been Successfully dropped\n");
+						pathArray.add((list.get(j)).toString());
+					}
+					event.dropComplete(true);
+					securedLockButton.setVisible(true);
+					justLockButton.setVisible(true);
+					return;
+				}
+			}
+			justDragDropAnyTextArea.append("Drop failed: " + event);
+			event.rejectDrop();
+		} catch (UnsupportedFlavorException e) {
+			e.printStackTrace();
+			event.rejectDrop();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 }
